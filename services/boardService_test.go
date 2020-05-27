551203baad1042a2ef672e4a7fa5e095f27f76ec
DSW2500 <DSW_25@outlook.com>
2020-05-27 13:51:54 +0530

    small changes

---
+++ services/boardService_test.go
@@ -0,0 +1,24 @@
+package services
+import (
+	"testing"
+	"ticgame/components"
+)
+
+func TestNewBoardService(t *testing.T){
+expected:= &BoardService{
+	board: components.CreateBoard(3),
+}
+var size uint8
+actual:= &Board{
+		Cells: []*Cell{
+			{Mark: NoMark},
+			{Mark: NoMark}, {Mark: NoMark}, {Mark: NoMark}, {Mark: NoMark}, {Mark: NoMark}, {Mark: NoMark}, {Mark: NoMark}, {Mark: NoMark}},
+		Size: 9,
+	}
+	var i uint8
+	for i=0;i<size;i++{
+	if actual.Board.Cells[i].Mark!=expected.Board.Cells[i].Mark {
+		t.Error("Error")
+	}
+
+}
